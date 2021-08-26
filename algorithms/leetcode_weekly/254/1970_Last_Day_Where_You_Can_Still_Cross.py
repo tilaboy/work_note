19db13e6a8231db6cf627cedf55ab719ee285c9d
Chao Li <chaoli@textkernel.nl>
2021-08-26 09:26:34 +0800

    updated 254 solution

--- algorithms/leetcode_weekly/254/1970_Last_Day_Where_You_Can_Still_Cross.py
+++ algorithms/leetcode_weekly/254/1970_Last_Day_Where_You_Can_Still_Cross.py
@@ -1,7 +1,73 @@
 from typing import List
 
+class UnionSets:
+    def __init__(self, elements):
+        self.nr_sets = len(elements)
+        self.par = [ele for ele in range(self.nr_sets)]
+        self.size = [1] * self.nr_sets
+
+    def __repr__(self):
+        return f'{self.par} => {self.size}'
+
+    def find(self, x):
+        root = x
+        while root != self.par[root]:
+            root = self.par[root]
+
+        while x != root:
+            next_x = self.par[x]
+            self.par[x] = root
+            x = next_x
+        return root
+
+    def union(self, x, y):
+        r_x = self.find(x)
+        r_y = self.find(y)
+        if r_x == r_y:
+            return
+
+        if self.size[r_x] > self.size[r_y]:
+            self.par[r_y] = r_x
+            self.size[r_x] = self.size[r_x] + self.size[r_y]
+        else:
+            self.par[r_x] = r_y
+            self.size[r_y] = self.size[r_x] + self.size[r_y]
+        self.nr_sets -= 1
+
+
 class Solution:
-    def latestDayToCross(self, row: int, col: int, cells: List[List[int]]) -> int:
+    def latestDayToCrossUnionFind(self, row: int, col: int, cells: List[List[int]]) -> int:
+        cells = [(cell[0] - 1, cell[1] - 1) for cell in cells]
+        terr = [[0] * col for _ in range(row)]
+        size_uds = row * col + 2
+        uds = UnionSets([i for i in range(size_uds)])
+        nr_land = 0
+        for i, j in cells[::-1]:
+            terr[i][j] = 1
+            nr_land += 1
+            if i > 0 and terr[i-1][j] == 1:
+                uds.union(i * col + j + 1, (i - 1) * col +j +1)
+            if i < row - 1 and terr[i+1][j] == 1:
+                uds.union(i * col + j + 1, (i + 1) * col +j +1)
+            if j > 0 and terr[i][j - 1] == 1:
+                uds.union(i * col + j + 1, i * col + j)
+            if j < col - 1 and terr[i][j + 1] == 1:
+                uds.union(i * col + j + 1, i * col + j + 2)
+
+            if i == 0:
+                uds.union(j + 1, 0)
+            if i == row - 1:
+                uds.union(i * col + j + 1, size_uds - 1)
+            if uds.find(0) == uds.find(size_uds - 1):
+                break
+        return len(cells) - nr_land
+
+
+
+
+
+
+    def latestDayToCrossBinarySearch(self, row: int, col: int, cells: List[List[int]]) -> int:
         def reachable(row, col, cells_to_last_move):
             reached = False
             visited = [[0]* col for i in range(row)]
@@ -62,6 +128,6 @@ cases = [
 ]
 
 for case in cases:
-    result = sol.latestDayToCross(case["input"][0], case["input"][1], case["input"][2])
+    result = sol.latestDayToCrossUnionFind(case["input"][0], case["input"][1], case["input"][2])
     print(case["input"], result)
     assert result == case['expect']
